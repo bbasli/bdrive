16086127c6028f00156463f5f9d29daaaf9c0d56
bbasli <bgrbasli@gmail.com>
2024-06-09 01:41:03 +0300

    Update page.tsx to use Clerk.js for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
-import Image from "next/image";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  useSession,
+} from "@clerk/nextjs";
 
 export default function Home() {
+  const session = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignInButton mode="modal" />
-      <Button>Hello World!</Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
