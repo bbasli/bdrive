4a86f1cf9634b3bb04ae9d0dcb31191826585010
bbasli <bgrbasli@gmail.com>
2024-06-10 04:21:14 +0300

    Move upload dialog to seperate component

---
+++ app/uploadButton.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { api } from "@/convex/_generated/api";
+
+import { useOrganization, useUser } from "@clerk/nextjs";
+
+import { useMutation } from "convex/react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((val) => val.length > 0, "Required"),
+});
+
+export default function UploadButton() {
+  const { toast } = useToast();
+  const organization = useOrganization();
+  const authState = useUser();
+
+  let orgId: string | undefined = undefined;
+
+  if (organization.isLoaded && authState.isLoaded) {
+    orgId = organization.organization?.id || authState.user?.id;
+  }
+
+  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false);
+
+  const uploadFile = useMutation(api.files.uploadFile);
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+
+  const fileRef = form.register("file");
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!orgId) return;
+
+    try {
+      const postUrl = await generateUploadUrl();
+
+      const selectedFile = values.file[0];
+
+      const result = await fetch(postUrl, {
+        method: "POST",
+        headers: { "Content-Type": selectedFile.type },
+        body: selectedFile,
+      });
+
+      const { storageId } = await result.json();
+
+      await uploadFile({
+        orgId,
+        fileId: storageId,
+        name: values.title,
+      });
+
+      form.reset();
+
+      setIsFileDialogOpen(false);
+      toast({
+        variant: "success",
+        title: "File uploaded successfully",
+        description: "Now you can see your file in the list.",
+      });
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Failed to upload file",
+        description: "Something went wrong. Please try again later.",
+      });
+    }
+  }
+
+  return (
+    <div className="flex justify-between items-center">
+      <h1 className="text-4xl font-bold">Your Files</h1>
+      <Dialog
+        open={isFileDialogOpen}
+        onOpenChange={(isOpen) => {
+          setIsFileDialogOpen(isOpen);
+          form.reset();
+        }}
+      >
+        <DialogTrigger asChild>
+          <Button onClick={() => setIsFileDialogOpen(true)}>Upload File</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="mb-8">Upload Your File</DialogTitle>
+            <DialogDescription>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="file"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>File</FormLabel>
+                        <FormControl>
+                          <Input type="file" {...fileRef} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" disabled={form.formState.isSubmitting}>
+                    {form.formState.isSubmitting ? (
+                      <div className="flex gap-2 items-center">
+                        Uploding ...
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      </div>
+                    ) : (
+                      "Upload"
+                    )}
+                  </Button>
+                </form>
+              </Form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
