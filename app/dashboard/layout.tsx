0a640899f3e61d0c6f155cf0fe98161c78584034
bbasli <bgrbasli@gmail.com>
2024-06-13 18:15:32 +0300

    Extract side navigation into separate component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,4 @@
-import Link from "next/link";
-
-import { FileIcon, StarIcon } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
+import SideNavigation from "./side-nav";
 
 export default function DashboardLayout({
   children,
@@ -12,21 +8,7 @@ export default function DashboardLayout({
   return (
     <main className="w-full pt-12">
       <div className="flex h-full">
-        <div className="w-[256px] flex justify-start flex-col bg-purple-200 gap-4 p-4">
-          <Link href="/dashboard/files">
-            <Button variant={"link"} className="flex gap-2">
-              <FileIcon size={24} />
-              View Files
-            </Button>
-          </Link>
-
-          <Link href="/dashboard/favorites">
-            <Button variant={"link"} className="flex gap-2">
-              <StarIcon size={24} />
-              Favorites
-            </Button>
-          </Link>
-        </div>
+        <SideNavigation />
 
         <div className="w-full mx-8">{children}</div>
       </div>
---
+++ app/dashboard/side-nav.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+import { FileIcon, StarIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+export default function SideNavigation() {
+  const pathname = usePathname();
+
+  return (
+    <div className="w-[256px] flex justify-start flex-col bg-purple-200 gap-4 p-4">
+      <Link href="/dashboard/files">
+        <Button
+          variant={"link"}
+          className={clsx("flex gap-2", {
+            "text-orange-500": pathname.includes("/dashboard/files"),
+          })}
+        >
+          <FileIcon size={24} />
+          View Files
+        </Button>
+      </Link>
+
+      <Link href="/dashboard/favorites">
+        <Button
+          variant={"link"}
+          className={clsx("flex gap-2", {
+            "text-orange-500": pathname.includes("/dashboard/favorites"),
+          })}
+        >
+          <StarIcon size={24} />
+          Favorites
+        </Button>
+      </Link>
+    </div>
+  );
+}
