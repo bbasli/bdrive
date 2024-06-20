af5e5071c6af88cdb1b55ed34f3b53cf46eb2f48
bbasli <bgrbasli@gmail.com>
2024-06-20 23:32:24 +0300

    Add separator before download button

--- app/dashboard/_components/file-card.tsx
+++ app/dashboard/_components/file-card.tsx
@@ -214,6 +214,7 @@ function FileCardActions({
               </>
             )}
           </Protect>
+          <DropdownMenuSeparator />
 
           {/* DOWNLOAD ACTION */}
           <DropdownMenuItem
