a7713235eb12d6ea4c0af76f20dca4a31eb3a5ba
bbasli <bgrbasli@gmail.com>
2024-06-21 21:42:59 +0300

    Add logo to header component

--- app/header.tsx
+++ app/header.tsx
@@ -7,13 +7,17 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 export default function Header() {
   return (
     <div className="border-b py-4 bg-gray-50">
       <div className="container mx-auto flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-gray-900">B-Drive</h1>
+        <div className="flex items-center gap-2">
+          <Image src={"/logo.png"} width={50} height={50} alt={"B-Drive"} />
+          <h1 className="text-2xl font-bold text-gray-900">B-Drive</h1>
+        </div>
         <div>
           <OrganizationSwitcher />
           <UserButton />
