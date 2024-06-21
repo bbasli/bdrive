a03eeee0ff13ff6f1aa8851f2a36284f2404fc58
bbasli <bgrbasli@gmail.com>
2024-06-21 21:51:34 +0300

    Wrap logo with link component

--- app/header.tsx
+++ app/header.tsx
@@ -8,16 +8,19 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
     <div className="border-b py-4 bg-gray-50">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Image src={"/logo.png"} width={50} height={50} alt={"B-Drive"} />
-          <h1 className="text-2xl font-bold text-gray-900">B-Drive</h1>
-        </div>
+        <Link href="/">
+          <div className="flex items-center gap-2">
+            <Image src={"/logo.png"} width={50} height={50} alt={"B-Drive"} />
+            <h1 className="text-2xl font-bold text-gray-900">B-Drive</h1>
+          </div>
+        </Link>
         <div>
           <OrganizationSwitcher />
           <UserButton />
